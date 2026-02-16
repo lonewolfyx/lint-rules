0b3963921d00cae8dbfbdbb4f3d6793ca8c6885a
lonewolfyx <877085893@qq.com>
2026-02-16 14:13:06 +0800

    fix(rules): update lint plugins component to use new data structure

--- apps/web/app/components/rules/LintPlugins.vue
+++ apps/web/app/components/rules/LintPlugins.vue
@@ -1,7 +1,11 @@
 <template>
     <ScrollArea class="overflow-hidden w-full min-h-full h-[calc(100vh-6rem)]">
         <div class="space-y-3">
-            <div class="flex w-full flex-col gap-2">
+            <div
+                v-for="lint in lintRules"
+                :key="lint.name"
+                class="flex w-full flex-col gap-2"
+            >
                 <div
                     :class="cn(
                         'text-sm bg-secondary relative text-foreground',
@@ -12,37 +16,19 @@
                     )"
                 >
                     <span class="font-medium">
-                        Built-in ESLint Rules
+                        {{ lint.name }}
                     </span>
                     <p class="text-xs text-muted-foreground font-normal truncate">
-                        Core rules included with ESLint by defaultCore rules included with ESLint by default
+                        {{ lint.description }}
                     </p>
                 </div>
-            </div>
-            <div
-                v-for="item in plugins"
-                :key="item.plugin"
-                :class="cn(
-                    'text-sm bg-secondary relative text-foreground',
-                    'rounded-md',
-                    'p-2 pl-6',
-                    'after:absolute after:inset-y-2 after:left-2 after:w-1',
-                    'after:rounded-full after:bg-violet-500',
-                )"
-            >
-                <span class="font-medium">
-                    {{ item.plugin }}
-                </span>
-                <p class="text-xs text-muted-foreground font-normal truncate">
-                    {{ item.description }} ({{ item.rules }} rules)
-                </p>
             </div>
         </div>
     </ScrollArea>
 </template>
 
 <script lang="ts" setup>
-import type { IPluginInfo } from '#shared/types/plugins'
+import type { ILintRulesData } from '#shared/types/rules'
 import { ScrollArea } from '@private/shadcn-vue/components/ui/scroll-area'
 import { cn } from '@private/shadcn-vue/lib/utils'
 
@@ -50,6 +36,6 @@ defineOptions({
     name: 'RulesLintPlugins',
 })
 
-const { data } = useLazyAsyncData('plugins', () => $fetch<{ data: IPluginInfo[] }>('/api/rule/plugins'))
-const plugins = computed(() => (data.value?.data ?? []).sort((a, b) => b.rules - a.rules))
+const { data } = useLazyAsyncData('plugins', () => $fetch<{ data: ILintRulesData[] }>('/api/rule/list'))
+const lintRules = computed(() => data.value?.data ?? [])
 </script>
